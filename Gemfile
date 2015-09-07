6983a350fda150a3ab68020ac7d02c26c64066a8
Ryan Buckley <arebuckley@gmail.com>
2015-09-06 19:11:45 -0700

    add pg for dev too

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
-  gem 'sqlite3'
 end
 
 gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150907003450) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reservations", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
