7532744bcf485842dd2a93802207861fd3ac65bb
Ryan Buckley <arebuckley@gmail.com>
2015-09-07 14:26:00 -0700

    add sendgrid

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'catamaran' => 'home#catamaran'
   get 'gallery' => 'home#gallery'
-
+  get 'contact' => 'reservations#new'
   resources :reservations, only: [:new, :create]
 end
