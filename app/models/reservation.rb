8320d965f44ab7fda4ac73c536f2e39db7ed33fb
Ryan Buckley <arebuckley@gmail.com>
2015-09-07 20:14:11 -0700

    rename method in reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,14 +1,12 @@
 class Reservation < ActiveRecord::Base
   attr_accessor :morning_discovery_cruise, :sunset_cocktail_cruise, :island_hopping
-
   validates :name, :message, :email,
             presence: true
   validates :email,
             format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})|[\d\s()+-.*]+\Z/i }
+  before_save :set_cruise_type
 
-  before_save :copy_cruise_options
-
-  def copy_cruise_options
+  def set_cruise_type
     write_attribute :cruise_type, %i[morning_discovery_cruise sunset_cocktail_cruise island_hopping].map { |attr|
       attr.to_s.humanize if public_send(attr) == '1'
     }.compact.join(', ')
