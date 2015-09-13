2bf293f5307ec076944b1e613b0c3aaf4c509560
Ryan Buckley <arebuckley@gmail.com>
2015-09-12 18:04:00 -0700

    add zoom .8 to all card media on mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def card_support_text(txt)
+    %Q(<div class="card_support meta color-text-blue">
+        <div class="grid">
+          <div class="left">
+            <div class="logo mini"></div>
+          </div>
+          <div class="row-10-col">
+            <div class="business-description">
+              <strong>#{txt}</strong>
+            </div>
+          </div>
+        </div>
+      </div>).html_safe
+  end
 end
