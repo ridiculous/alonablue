10f6f0cdec1a0697914e57af70c0b1d6622eba44
Ryan Buckley <arebuckley@gmail.com>
2015-09-26 13:44:46 -0700

    add links to tripadvisor and facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,12 @@ module ApplicationHelper
   end
 
   def page_title
-    "#{@page_title} | Alona Blue Charters".sub(/^[|\s]*/, '')
+    "#{@page_title} | Alona Blue Sailing Charters".sub(/^[|\s]*/, '')
+  end
+
+  def google_analytics_script
+    %Q^
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-42812390-1', 'auto');ga('send', 'pageview');</script>
+    ^.html_safe
   end
 end
